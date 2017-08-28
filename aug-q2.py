f8cab3d4f2b3db6ea1ffd6242a57d88a6747fcca
working4happy <imlinbin@msn.com>
2017-08-28 21:32:03 +0800

    add codes for check_d is 'B'

--- aug-q2.py
+++ aug-q2.py
@@ -4,6 +4,7 @@
 
 
 def cal_count(s, pos, p, pos_len, a, c):
+    # s is the list of the characters in an ID
     import calendar
     i = p + 1
     count = c
@@ -46,10 +47,9 @@ def cal_a(ss):
 
 import time
 
-id_1 = '111111111B111111116'
+id_1 = '1B11111111111111116'
 start = time.clock()
-if id_1[-1] != 'B':
-    a = int(id_1[-1])
+
 lst = range(19)
 for i in range(19):
     if i == 0:
@@ -71,23 +71,41 @@ for i in range(19):
     for li, lv in enumerate(lst[i]):
         lst[i][li] = str(lv)
 
-s = []
-for j in range(len(id_1)):
-    s.append(id_1[j])
 
-part_1 = id_1[:8]
-part_2 = id_1[8:18]
+
 check_d = id_1[-1]
 
-num_b = id_1.count('B')
-pos_b = []
-pos_b1 = None
+if check_d != 'B':
+    num_b = id_1.count('B')
+    pos_b = []
 
-if num_b >= 1:
-    pos_b1 = id_1.find('B')
-    for i in range(num_b):
-        pos_b.append(id_1.find('B',pos_b1+i))
+    if num_b >= 1:
+        pos_b.append(id_1.find('B'))
+        for i in range(1, num_b):
+            pos_b.append(id_1.find('B',pos_b[i-1]+1))
+    print pos_b # to be deleted
 
-cc = cal_count(s, pos_b, 0, len(pos_b), a, 0)
-print 'cc:',cc, time.clock()-start
-#print cal_a(s)
+    s = []
+    for j in iter(id_1):
+        s.append(j)
+    cc = cal_count(s, pos_b, 0, len(pos_b), check_d, 0)
+    print 'cc:',cc, time.clock()-start # to be deleted
+else:
+    part_1 = id_1[:8]
+    part_2 = id_1[8:18]
+    num_b_p2 = part_2.count('B')
+    count_p2 = 10**num_b_p2
+
+    pos_b_p1 = []
+    num_b_p1 = part_1.count('B')
+    if num_b_p1 >=1:
+        pos_b_p1.append(part_1.find('B'))
+        for y in range(1, num_b_p1):
+            pos_b_p1.append(part_1.find('B',pos_b_p1[y-1]+1))
+
+        s_p1 = []
+        for z in iter(part_1):
+            s_p1.append(z)
+        count_p1 = part_count() # a new function need be defined
+    else:
+        count_p1 = 1
